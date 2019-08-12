5053655c4cf7fe83093a5f0cc75adf90a573186f
z-jack <emailjiong@126.com>
2019-08-12 14:45:03 +0800

    finish read PDF

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,91 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Tabs type="card" closable :on-tab-remove="closePDF">
+      <TabPane v-for="tab in files" :key="tab.name" :label="tab.name">test</TabPane>
+      <Button
+        @click="handleTabsAdd"
+        size="small"
+        type="primary"
+        shape="circle"
+        icon="ios-add"
+        slot="extra"
+      ></Button>
+    </Tabs>
+    <Upload
+      multiple
+      ref="pdfUploader"
+      v-show="!files.length"
+      style="flex-grow:1"
+      type="drag"
+      action="/"
+      accept=".pdf"
+      :format="['pdf']"
+      :before-upload="readPDF"
+    >
+      <div style="padding: 20px 0">
+        <Icon type="ios-cloud-upload" size="74" style="color: #3399ff"></Icon>
+        <p style="font-size:36px">Click or drag PDF files here</p>
+      </div>
+    </Upload>
   </div>
 </template>
 
+<script>
+import PDF from "pdfjs-dist";
+import { Promise } from 'q';
+
+export default {
+  data () {
+    return {
+      files: []
+    }
+  },
+  methods: {
+    handleTabsAdd () {
+      this.$refs.pdfUploader.$el.children[0].click()
+    },
+    closePDF () {
+
+    },
+    readPDF (e) {
+      const reader = new FileReader()
+      reader.onload = async () => {
+        const pdf = await PDF.getDocument(reader.result)
+        const pages = await Promise.all(Array(pdf.numPages).fill(0).map((_, i) => pdf.getPage(i + 1)))
+        const textContexts = await Promise.all(pages.map(page => page.getTextContent()))
+        const naiveText = textContexts.map(c => c.items.map(t => t.str).join('')).join('')
+        console.log(naiveText)
+      }
+      reader.readAsArrayBuffer(e)
+      return false
+    }
+  }
+}
+</script>
+
 <style>
+body {
+  height: 100vh;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+  height: 100%;
+  padding: 12px;
+  display: flex;
+  flex-direction: column;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.ivu-upload {
+  flex-grow: 1;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  align-content: center;
+  justify-content: space-around;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.ivu-upload-list {
+  display: none;
 }
 </style>
